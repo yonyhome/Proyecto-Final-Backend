a1aa454e3496aa53ca4c236a9a4ad7962794630f
mavalero010 <37789587+mavalero010@users.noreply.github.com>
2022-11-26 12:28:25 -0500

    Anexo de rutas categoria producto

--- src/index.js
+++ src/index.js
@@ -7,10 +7,13 @@ const port=9001
 const  app = express()
 const userRoutes = require("./routes/UserRoute")
 const productRoutes = require("./routes/ProductRoute")
+const CatProRoutes = require("./routes/CategoriaProductoRoute")
+
 //Middleware 
 app.use(express.json())
 app.use('/api',userRoutes)
 app.use('/api',productRoutes)
+app.use('/api',CatProRoutes)
 
 // routes 
 app.get('/',(req,res) =>{
